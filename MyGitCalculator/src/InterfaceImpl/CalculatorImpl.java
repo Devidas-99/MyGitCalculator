ff2c49e2f462e8dde2bd33f9545eab64fc285856
DEVA <DEVA@192.168.1.65>
2025-07-04 16:20:31 +0530

    Commit first calculator

---
+++ MyGitCalculator/src/InterfaceImpl/CalculatorImpl.java
@@ -0,0 +1,15 @@
+package InterfaceImpl;
+
+import Interface.Calculator;
+
+public class CalculatorImpl implements Calculator {
+
+	@Override
+	public int sum(int a, int b){
+		  int sum = a+b;
+		return sum;
+	}
+	
+	
+
+}
