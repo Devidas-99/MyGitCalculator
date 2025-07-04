ee591d1e19e0ae2520715ca283765cba061e0499
DEVA <DEVA@LAPTOP-0NN4R7OE>
2025-07-04 19:54:40 +0530

    Addition and multiplication functionality implemented

--- MyGitCalculator/src/Interface/Calculator.java
+++ MyGitCalculator/src/Interface/Calculator.java
@@ -2,6 +2,9 @@ package Interface;
 
 public interface Calculator {
 
+	public int add(int a , int b);
+	
+	public int mul(int x , int y);
 
 
 }
--- MyGitCalculator/src/Test/Demo.java
+++ MyGitCalculator/src/Test/Demo.java
@@ -1,9 +1,17 @@
 package Test;
+import Interface.Calculator;
+import InterfaceImpl.CalculatorImpl;
 
 public class Demo {
 	public static void main(String[] args) {
+	
+		Calculator c = new CalculatorImpl();
 		
+		int add = c.add(12, 15);
+		System.out.println(add);
 		
+		int mul = c.mul(12, 5);
+		System.out.println(mul);
 		
 		
 		
